d37fe134c8947e2d76988a584100a1f5eefb87ca
Vidianos-Giannitsis <vidianosgiannitsis@gmail.com>
2022-12-19 20:59:38 +0200

    Adding docstrings and the literate config of org-roam-similarity

--- org-roam-similarity.el
+++ org-roam-similarity.el
@@ -42,20 +42,43 @@
 
 ;;; Code:
 
-(defvar org-roam-similarity-language "english")
+(defcustom org-roam-similarity-language "english"
+  "The language org-roam-similarity uses.
 
-(defvar org-roam-similarity-number-of-documents 10)
+Specifically, in the python script that does the heavy lifting,
+the nltk program is used for text pre-processing and this
+variable defines the language it should use. Defaults to english
+for obvious reasons."
+  :type 'string
+  :group 'org-roam-similarity)
 
-(defvar org-roam-similarity-directory org-directory)
+(defcustom org-roam-similarity-number-of-documents 10
+  "Amount of documents org-roam-similarity should return."
+  :type 'integer
+  :group 'org-roam-similarity)
 
-(defvar org-roam-similarity-show-scores nil)
+(defcustom org-roam-similarity-directory org-roam-directory
+  "Directory in which org-roam-similarity should look.
 
-(defvar org-roam-similarity-root
+Defaults to `org-roam-directory' as this package is meant to be
+used with org-roam."
+  :type 'directory
+  :group 'org-roam-similarity)
+
+(defcustom org-roam-similarity-show-scores nil
+  "Show similarity scoring of files in the output."
+  :type 'boolean
+  :group 'org-roam-similarity)
+
+(defcustom org-roam-similarity-root
   (concat (file-name-directory
-           (f-full (or
-                    load-file-name
-                    buffer-file-name)))
-          "."))
+	   (f-full (or
+		    load-file-name
+		    buffer-file-name)))
+	  ".")
+  "Root directory of org-roam-similarity."
+  :type 'directory
+  :group 'org-roam-similarity)
 
 (defun org-roam-similarity-insert-list ()
   "Insert a list of 'org-mode' links to files that are similar to the buffer file."
@@ -63,14 +86,38 @@
   (end-of-buffer)
   (newline)
   (let ((command (format "python3 %s -i %s -d %s -l %s -n %s %s"
-                         (concat org-roam-similarity-root "/assets/org-similarity.py")
-                         buffer-file-name
-                         org-roam-similarity-directory
-                         org-roam-similarity-language
-                         org-roam-similarity-number-of-documents
-                         (if org-roam-similarity-show-scores "--score" ""))))
+			 (concat org-roam-similarity-root "/assets/org-similarity.py")
+			 buffer-file-name
+			 org-roam-similarity-directory
+			 org-roam-similarity-language
+			 org-roam-similarity-number-of-documents
+			 (if org-roam-similarity-show-scores "--score" ""))))
     (insert (shell-command-to-string command)))
   (pop-global-mark)
+  )
+
+(defun org-roam-similarity-sidebuffer ()
+    "Puts the results of org-similarity in a side-window."
+    (interactive)
+    (let ((command (format "python3 %s -i %s -d %s -l %s -n %s %s"
+	    (concat org-similarity-root "/assets/org-similarity.py")
+	     buffer-file-name
+	     org-similarity-directory
+	     org-similarity-language
+	     org-similarity-number-of-documents
+	     (if org-similarity-show-scores "--score" ""))))
+      (setq similarity-results (shell-command-to-string command)))
+      (with-output-to-temp-buffer "*Similarity Results*"
+      (princ similarity-results))
+      (with-current-buffer "*Similarity Results*"
+      (org-mode))
+    )
+  (add-to-list 'display-buffer-alist
+	       '("*Similarity Results*"
+		 (display-buffer-in-side-window)
+		 (inhibit-same-window . t)
+		 (side . right)
+		 (window-width . 0.4))
   )
 
 (provide 'org-roam-similarity)
