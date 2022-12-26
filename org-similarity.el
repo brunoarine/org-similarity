b5c7d2f60b1f01efe0a181235c1b215d2624ca14
Bruno Arine <barine@runbox.com>
2022-12-26 11:12:34 -0300

    Add extra options in elisp code

--- org-similarity.el
+++ org-similarity.el
@@ -1,8 +1,8 @@
 ;;; org-similarity.el --- Org-mode Similarity Discovery -*- lexical-binding: t; -*-
 
 ;; Author: Bruno Arine <bruno.arine@runbox.com>
-;; URL: https://github.com/soldeace/org-similarity
-;; Version: 0.1
+;; URL: https://github.com/brunoarine/org-similarity
+;; Version: 0.2
 ;; Package-Requires: ((org "9.3") (emacs "26.1")))
 
 ;; 3-clause BSD License
@@ -41,33 +41,111 @@
 
 ;;; Code:
 
-(defvar org-similarity-language "english")
 
-(defvar org-similarity-number-of-documents 10)
+(require 'f)
 
-(defvar org-similarity-directory org-directory)
+(defgroup org-similarity nil
+  "Org-similarity."
+  :prefix "org-similarity-"
+  :group 'applications
+  :link '(url-link :tag "GitHub" "https://github.com/brunoarine/org-similarity")
+  :link '(emacs-commentary-link :tag "Commentary" "org-similarity"))
 
-(defvar org-similarity-show-scores nil)
+(defconst org-similarity-version "0.2"
+  "The current version of ORG-SIMILARITY.")
 
-(defvar org-similarity-root
-  (concat (file-name-directory
-           (f-full (or
-                    load-file-name
-                    buffer-file-name)))
-          "."))
+(defcustom org-similarity-language
+  "english"
+  "The language passed to nltk's Snowball stemmer."
+  :type 'string)
+
+(defcustom org-similarity-number-of-documents
+  10
+  "How many similar entries to list at the end of the buffer."
+  :type 'integer)
+
+(defcustom org-similarity-recursive-search
+  nil
+  "Whether to perform recursive file search."
+  :type 'boolean)
+
+(defcustom org-similarity-directory
+  org-directory
+  "Directory to scan for possibly similar documents."
+  :type 'string)
+
+(defcustom org-similarity-show-scores
+  nil
+  "Whether to prepend the list entries with their cosine similarity score."
+  :type 'boolean)
+
+(defcustom org-similarity-id-links
+  nil
+  "Whether to show results as ID links instead of FILE links."
+  :type 'boolean)
+
+(defvar org-similarity-directory
+  (file-name-directory
+   (f-full (or load-file-name buffer-file-name)))
+  "Org-similarity location.")
+
+(defun org-similarity--python-is-available ()
+  "Return t if Python is available."
+  (unless (executable-find "python")
+    (error "Org-similarity needs Python to run. Please, install Python"))
+  t)
+
+(defvar org-similarity-deps-install-buffer-name
+  " *Install org-similarity Python dependencies* "
+  "Name of the buffer used for installing org-similarity dependencies.")
+
+(defcustom org-similarity-python-interpreter-path
+  "python"
+  "Path to the Python executable that you want to use."
+  :type 'string)
+
+(defun org-similarity-install-dependencies ()
+  "Create environment and install Python dependencies and main script."
+  (when (org-similarity--python-is-available)
+(let* ((install-commands
+        (concat
+         "cd " org-similarity-directory "&& \
+         python -m venv venv && \
+         source venv/bin/activate && \
+         python -m pip install --upgrade pip && \
+         python -m pip install -r requirements.txt && \
+         cd -"))
+       (buffer (get-buffer-create org-similarity-deps-install-buffer-name)))
+      (pop-to-buffer buffer)
+      (compilation-mode)
+      (if (zerop (let
+                     ((inhibit-read-only t))
+                   (call-process "sh" nil buffer t "-c" install-commands)))
+          (message "Installation of `org-similarity' Python dependencies succeeded")
+        (error "Installation of `org-similarity' Python dependencies failed!")))))
+(org-similarity-install-dependencies)
+
+;; If org-similarity dependencies are not installed yet, install them
+(unless (require 'org-similarity nil t)
+  (if (y-or-n-p "Org-similarity needs to download some Python packages to work. Download them now? ")
+      (progn
+        (org-similarity-install-dependencies)
+        (require 'org-similarity))
+    (error "Org-similarity won't work until its Python dependencies are downloaded!")))
 
 (defun org-similarity-insert-list ()
   "Insert a list of 'org-mode' links to files that are similar to the buffer file."
   (interactive)
   (end-of-buffer)
   (newline)
-  (let ((command (format "python3 %s -i %s -d %s -l %s -n %s %s"
-                         (concat org-similarity-root "/assets/org-similarity.py")
+  (let ((command (format "%s -m org-similarity -i %s -d %s -l %s -n %s %s"
+                         org-similarity-python-interpreter-path
                          buffer-file-name
                          org-similarity-directory
                          org-similarity-language
                          org-similarity-number-of-documents
-                         (if org-similarity-show-scores "--score" ""))))
+                         (if org-similarity-show-scores "--scores" "")
+                         (if org-similarity-recursive-search "--recursive"))))
     (insert (shell-command-to-string command)))
   (pop-global-mark)
   )
