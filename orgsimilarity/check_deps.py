ef9abaecb8ff0e96f8a6d67d30a3982ce6b4626d
Bruno Arine <barine@runbox.com>
2022-12-29 00:45:36 -0300

    Fix reqs path in check_deps after file move
    * orgsimilarity/check_deps.py:

--- orgsimilarity/check_deps.py
+++ orgsimilarity/check_deps.py
@@ -1,5 +1,5 @@
 import pkg_resources
 from pathlib import Path
 
-requirements_file = Path(__file__).parent / "requirements.txt"
+requirements_file = Path(__file__).parent.parent / "requirements.txt"
 pkg_resources.require(open(requirements_file, mode="r"))
