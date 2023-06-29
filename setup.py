a0f8f3832bbc894bf81b16810892f7a8648f53f4
Bruno Arine <bruno.arine@runbox.com>
2023-06-29 09:49:33 -0300

    fix: upgrade to v1.0.0 in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="org-similarity",
-    version="0.3",
+    version="1.0.0",
     author="Bruno Arine",
     author_email="bruno.arine@runbox.com",
     packages=["orgsimilarity"],
